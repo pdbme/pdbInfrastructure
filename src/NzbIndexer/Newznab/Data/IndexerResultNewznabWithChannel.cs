3f278313ceb5fd0f86a02b9afde04b99bbed430a
pdbme <67370294+pdbme@users.noreply.github.com>
2023-04-07 09:41:53 +0200

    Newznab With Channel for nzbfinder

---
+++ src/NzbIndexer/Newznab/Data/IndexerResultNewznabWithChannel.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace pdbme.pdbInfrastructure.NzbIndexer.Newznab.Data;
+
+public class IndexerResultNewznabWithChannel
+{
+    [JsonPropertyName("channel")]
+    public IndexerResultNewznab? Channel { get; set; }
+}
